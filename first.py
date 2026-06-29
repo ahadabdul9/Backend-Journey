2d979452b9f58f05f6742cfac4acc522519ff83a
ABDUL AHAD <ahad.abdul1025@gmail.com>
2026-06-29 06:20:16 +0530

    Day 1: Python fundamentals

---
+++ first.py
@@ -0,0 +1,30 @@
+# a = 3
+# b = 4
+# sum = a + b
+# print(sum)
+# a += 10
+# print(a)
+
+# name = input("Enter your name : ")
+# print("welcome ", name)
+
+
+# # write a program to input 2 numbers & print their sum
+
+# a = int (input ("enter a number : "))
+# b = int (input ("enter a number : "))
+# sum = a + b
+# print(sum)
+
+
+# # write a program to input side of a square & print its area.
+
+# side = float(input("enter side : "))
+# print("area =", side ** 2)
+
+# write a program to input 2 floating point numbers & print their avg
+
+a = float(input("Enter : "))
+b = float(input("Enter : "))
+avg = (a+b)/2
+print (avg)
